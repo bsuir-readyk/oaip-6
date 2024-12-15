7ff165813d2f6840328e6014ac4c1664844470c0
readyyyk <mr.sairexer@gmail.com>
2024-12-15 04:02:07 +0300

    some naming changes

--- solve.dpr
+++ solve.dpr
@@ -1,16 +1,16 @@
-const n = 6;
-Type TGmas = array[1..n] of array[1..n] of integer;
-Type Tmas = array[1..100] of integer;
+const N = 6;
+Type TGraphMatrix = array[1..N] of array[1..N] of integer;
+Type TArr = array[1..100] of integer;
 
-var G: TGmas =
+var GRAPH: TGraphMatrix =
 {
 (
-    (0, 1, 1, 1, 1, 1),
-    (1, 0, 1, 1, 1, 1),
-    (1, 1, 0, 1, 1, 1),
-    (1, 1, 1, 0, 1, 1),
-    (1, 1, 1, 1, 0, 1),
-    (1, 1, 1, 1, 1, 0)
+  (0, 1, 1, 1, 1, 1),
+  (1, 0, 1, 1, 1, 1),
+  (1, 1, 0, 1, 1, 1),
+  (1, 1, 1, 0, 1, 1),
+  (1, 1, 1, 1, 0, 1),
+  (1, 1, 1, 1, 1, 0)
 );
 }
 {
@@ -75,10 +75,10 @@ var G: TGmas =
 
 
 // Функция нахождения максимальной клики
-function FindMaxClick(G: TGmas; k, x: integer; var maxVer: integer; curVers: Tmas; var maxCL, curCl: Tmas): Tmas;
+function FindMaxClick(GRAPH: TGraphMatrix; k, x: integer; var maxVer: integer; curVers: TArr; var maxCL, curCl: TArr): TArr;
 var
   i, j, p, l: integer;
-  curVers2: Tmas;
+  curVers2: TArr;
   b: boolean;
 begin
   if ((k = 1) or (k = 0)) then
@@ -102,7 +102,7 @@ begin
       p := 1;
       while (j <= k) do
       begin
-        if (G[curVers[i], curVers[j]] = 1) then
+        if (GRAPH[curVers[i], curVers[j]] = 1) then
           begin
             curVers2[p] := curVers[j];
             p := p + 1;
@@ -114,7 +114,7 @@ begin
         b := True;
         curCl[x] := curVers2[1];
         x := x + 1;
-        curCl := FindMaxClick(G, p-1, x, maxVer, curVers2, maxCl, curCl);
+        curCl := FindMaxClick(GRAPH, p-1, x, maxVer, curVers2, maxCl, curCl);
       end;
       if (maxVer < x-1) then
       begin
@@ -131,18 +131,18 @@ end;
 var
   i, j, k, x, l, h: integer;
   maxVer, maxVerRemember, numMaxCl: integer;
-  curCl, maxCl, curVers, maxClNew: Tmas;
+  curCl, maxCl, curVers, maxClNew: TArr;
 begin
   i := 1;
   maxVerRemember := 1;
   maxCl[1] := 1;
   numMaxCl := 0;
-  while (i < n) do
+  while (i < N) do
   begin
-    // Обнуление прошлых элементов
-    for l := 1 to n do
+    // Онуление прошлых элементов
+    for l := 1 to N do
       curVers[l] := 0;
-    for l := 3 to n do
+    for l := 3 to N do
       curCl[l] := 0;
     curCl[1] := i;
     j := i + 1;
@@ -150,13 +150,13 @@ begin
 
     // Нахождение соседей
     repeat
-      if (G[i, j] = 1) then
+      if (GRAPH[i, j] = 1) then
         begin
           curVers[k] := j;
           k := k + 1;
         end;
       j := j + 1;
-    until (j > n);
+    until (j > N);
     if (curVers[1] <> 0) then
     begin
       curCl[2] := curVers[1];
@@ -164,7 +164,7 @@ begin
       x := 3;
       maxClNew := curCl;
 
-      maxClNew := FindMaxClick(G, k-1, x, maxVer, curVers, maxClNew, curCl);
+      maxClNew := FindMaxClick(GRAPH, k-1, x, maxVer, curVers, maxClNew, curCl);
 
       // Обновление максимальной клики
       if (maxVerRemember < maxVer) then
@@ -185,7 +185,7 @@ begin
 
   // Вывод максимальной клики
   if (maxVerRemember = 1) then
-    for l := 1 to n do
+    for l := 1 to N do
     writeln(l)  
   else if (maxVerRemember < numMaxCl) then
   begin
